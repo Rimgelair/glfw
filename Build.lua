0bf0717612f4c62e8f8b5f4039990ab3e6ff9994
Herbert Yanes <rimgelair@gmail.com>
2025-01-31 01:13:14 -0500

    Update Build.lua
    fix build issues

--- Build.lua
+++ Build.lua
@@ -7,48 +7,64 @@ project "GLFW"
     targetdir("%{wks.location}/bin/" .. OutputDir .. "/%{prj.name}")
     objdir("%{wks.location}/bin/int" .. OutputDir .. "/%{prj.name}")
 
-    files
-    {
-        "include/GLFW/glfw3.h",
-        "include/GLFW/glfw3native.h",
-
+    files {
         "src/context.c",
         "src/init.c",
         "src/input.c",
         "src/monitor.c",
-        "src/internal.h",
-        "src/platform.h",
-        "src/vulkan.h",
+        "src/vulkan.c",
         "src/window.c",
-        "src/egl_context.c",
-        "src/osmesa_context.c",
-
-        "src/null_platform.h",
-        "src/null_joystick.h",
+        "src/platform.c",
         "src/null_init.c",
         "src/null_monitor.c",
         "src/null_window.c",
-        "src/null_joystick.c",
+        "src/null_joystick.c"
     }
 
-	filter "system:macosx"
-		pic "On"
+    includedirs {
+        "include"
+    }
 
-		files
-		{
-            "src/cocoa_platform.h",
-            "src/cocoa_joystick.h",
+    filter "system:windows"
+        systemversion "latest"
+        
+        files {
+            "src/win32_init.c",
+            "src/win32_joystick.c",
+            "src/win32_monitor.c",
+            "src/win32_time.c",
+            "src/win32_thread.c",
+            "src/win32_module.c",
+            "src/win32_window.c",
+            "src/wgl_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c"
+        }
+
+        defines { 
+            "_GLFW_WIN32",
+            "_CRT_SECURE_NO_WARNINGS"
+        }
+
+    filter "system:macosx"
+        systemversion "latest"
+        
+        files {
             "src/cocoa_init.m",
             "src/cocoa_joystick.m",
             "src/cocoa_monitor.m",
             "src/cocoa_window.m",
+            "src/cocoa_time.c",
+            "src/posix_thread.c",
+            "src/posix_module.c",
             "src/nsgl_context.m",
-		}
+            "src/egl_context.c",
+            "src/osmesa_context.c"
+        }
 
-		defines
-		{
-			"_GLFW_COCOA"
-		}
+        defines { 
+            "_GLFW_COCOA"
+        }
 
 		links
 		{
